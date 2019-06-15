81c208db198e16536ebbeb23ccb8ddf53598e727
Bonggt <kepy1106@gmail.com>
2019-06-15 18:58:44 +0900

    onChange, onSubmit 이벤트를 다루는 방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 import Number from "./Number";
+import { Form, Input } from "./Input";
 
 interface IState {
   counter: number;
+  name: string;
 }
 class App extends Component<{}, IState> {
   state = {
-    counter: 0
+    counter: 0,
+    name: ""
   };
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <>
+        <Form onSubmit={this.onSubmitForm}>
+          <Input value={name} onChange={this.onInputChange} />
+        </Form>
         <Number count={counter} />
         <button onClick={this.add}>Add</button>
       </>
     );
   }
+
+  onInputChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onSubmitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("엔터");
+  };
+
   add = (): void => {
     this.setState(prevState => {
       return {
